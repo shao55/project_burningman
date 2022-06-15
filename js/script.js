8d125a991d41e85d048f58fe55c40217b42061bb
Nurgissa <nurgissa.22@gmail.com>
2022-06-15 15:13:43 +0600

    First load

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// let admin, name;
+// name = "Джон";
+// admin = name;
+// let ourPlanetName = "Земля";
+// let currentUserName = "Нургиса";
+// const BIRTHDAY = "06.06.1993";
+// const COLOR_RED = "#F00";
+// const COLOR_GREEN = "#0F0";
+// const COLOR_BLUE = "#00F";
+// const COLOR_ORANGE = "#FF7F00";
+
+// let color = COLOR_BLUE
+// alert (admin);
+
